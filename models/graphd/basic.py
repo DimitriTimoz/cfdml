bf8bbd58b75bb629cec7277b0f73544a7e9db646
DimitriTimoz <dimitri.timoz@protonmail.com>
2024-09-22 19:17:18 +0200

    New GANN gen

--- models/graphd/basic.py
+++ models/graphd/basic.py
@@ -3,75 +3,22 @@ import os
 
 from tqdm import tqdm
 import numpy as np
+
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch import Tensor
-from torch_geometric.loader import DataLoader
-from torch_geometric.data import Data, Batch
+import torch.nn.functional as F
+from torch.nn import Conv1d, ReLU, MaxPool1d, ReLU, Dropout
 from torch_geometric.nn import SAGEConv
+from torch_geometric.loader import DataLoader
+from torch_geometric.data import Data
 
-from torch_geometric.transforms import BaseTransform
-from scipy.spatial import Delaunay
 from sklearn.utils.validation import check_is_fitted
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
-import numpy as np
 
-import matplotlib.pyplot as plt
+from parser import load_edges
 
-
-class DelaunayTransform(BaseTransform):
-    def __init__(self, dim=2):
-        """
-        Initialize the DelaunayTransform.
-
-        Args:
-            dim (int): Dimensionality of the points (default is 2 for 2D).
-        """
-        self.dim = dim
-
-    def __call__(self, data: Data) -> Data:
-        """
-        Apply Delaunay triangulation to the node coordinates to construct edge_index.
-
-        Args:
-            data (Data): PyTorch Geometric Data object with 'x' attribute.
-
-        Returns:
-            Data: Updated Data object with 'edge_index' constructed via Delaunay triangulation.
-        """            
-        # Convert node features to NumPy array
-        points = data.pos
-
-        # Perform Delaunay triangulation
-        tri = Delaunay(points)
-        # Extract edges from the simplices
-        edges = set()
-        for simplex in tri.simplices:
-            # Each simplex is a triangle represented by three vertex indices
-            edges.add(tuple(sorted([simplex[0], simplex[1]])))
-            edges.add(tuple(sorted([simplex[0], simplex[2]])))
-            edges.add(tuple(sorted([simplex[1], simplex[2]])))
-
-        # Convert set of edges to a list
-        edge_index = np.array(list(edges)).T  # Shape: (2, num_edges)
-
-        # Convert edge_index to torch tensor
-        edge_index = torch.tensor(edge_index, dtype=torch.long)
-
-        # Optionally, you can compute edge attributes here (e.g., Euclidean distances)
-        # For example:
-        # edge_attr = torch.norm(data.x[edge_index[0]] - data.x[edge_index[1]], dim=1, keepdim=True)
-        # data.edge_attr = edge_attr
-
-        # Update the Data object
-        data.edge_index = edge_index
-        data.edge_attr = np.zeros((edge_index.shape[1], 1))
-
-        return data
-
-
-class GraphSAGE(torch.nn.Module):
+class GraphD(torch.nn.Module):
     def __init__(self, device, in_dim, hidden_dims: list, out_dim, dropout=0.2):
         super().__init__()
         self.device = device
@@ -82,27 +29,28 @@ class GraphSAGE(torch.nn.Module):
             self.convs.append(SAGEConv(in_dim, hidden_dim).to(self.device))
             in_dim = hidden_dim
     
-    def forward(self, data: Batch) -> Tensor: 
+    def forward(self, data) -> Tensor: 
         x = data.x.to(self.device)
         edge_index = data.edge_index.to(self.device)
         for conv in self.convs[:-1]:
-            x = F.elu(conv(x, edge_index))
+            x = F.relu(conv(x, edge_index))
             x = F.dropout(x, p=self.dropout)
             
         x = self.convs[-1](x, edge_index)
         return x # TODO: choose activation function according to the variable
             
-
 class BasicSimulator(nn.Module):
-    def __init__(self, **kwargs):
+    def __init__(self, benchmark, **kwargs):
         super().__init__()
         self.name = "AirfRANSSubmission"
         use_cuda = torch.cuda.is_available()
         self.device = 'cuda:0' if use_cuda else 'cpu'
-        self.model = GraphSAGE(self.device, 5, [128, 256, 256, 128], 4)
+        self.model = GraphD(self.device, 5, [128, 256, 256, 128], 4)
         self.scaler = StandardScaler(copy=False)
         self.target_scaler = MinMaxScaler(copy=False)
         self.hparams = kwargs
+        self.benchmark_path = benchmark.benchmark_path
+
         if use_cuda:
             print('Using GPU')
         else:
@@ -114,7 +62,6 @@ class BasicSimulator(nn.Module):
         coord_x=dataset.data['x-position']
         coord_y=dataset.data['y-position']
         surf_bool=dataset.extra_data['surface']
-
         position = np.stack([coord_x,coord_y],axis=1)
 
         nodes_features, node_labels = dataset.extract_data()
@@ -133,39 +80,41 @@ class BasicSimulator(nn.Module):
         else:
             print("Scale not train data")
             nodes_features = self.scaler.transform(nodes_features)
-            node_labels = self.target_scaler.transform(node_labels)
-        print("Transform done")
+            node_labels = self.target_scaler.transform(node_labels)        
 
-        transform = DelaunayTransform() 
-
+        print("Extracting edges")
+        edges = load_edges(dataset, self.benchmark_path, training)
+        print("Edges extracted")
         torchDataset=[]
         nb_nodes_in_simulations = dataset.get_simulations_sizes()
         start_index = 0
         print("Start processing dataset")
         i = 0
         for nb_nodes_in_simulation in nb_nodes_in_simulations:
-            i += 1
             end_index = start_index+nb_nodes_in_simulation
             simulation_positions = torch.tensor(position[start_index:end_index,:], dtype = torch.float) 
             simulation_features = torch.tensor(nodes_features[start_index:end_index,:], dtype = torch.float) 
             simulation_labels = torch.tensor(node_labels[start_index:end_index,:], dtype = torch.float) 
             simulation_surface = torch.tensor(surf_bool[start_index:end_index])
-
+            print("Simulation ", i, " with ", nb_nodes_in_simulation, " nodes")
+            print("Nodes in the file", edges[i][1])
+            assert edges[i][1] == nb_nodes_in_simulation
             sampleData = Data(pos=simulation_positions,
                             x=simulation_features, 
                             y=simulation_labels,
+                            edges=edges[i][0],
                             surf = simulation_surface.bool()) 
-            sampleData = transform(sampleData)
             torchDataset.append(sampleData)
             start_index += nb_nodes_in_simulation
+            i += 1
         return DataLoader(dataset=torchDataset, batch_size=1)
 
     def train(self, train_dataset, save_path=None, local=False):
         print("Training")
+
         if local:
             # Check if file exists
             if os.path.exists("train_dataset_meshed.pth"):
-                # Load the dataset
                 train_dataset = torch.load("train_dataset_meshed.pth")
             else:
                 # Process the dataset
@@ -178,6 +127,7 @@ class BasicSimulator(nn.Module):
         # Save the model
         if local:
             torch.save(self, "model.pth")
+        torch.save(self, "model.pth")
 
     def predict(self, dataset, **kwargs):
         test_dataset = self.process_dataset(dataset, training=False)
@@ -196,7 +146,7 @@ class BasicSimulator(nn.Module):
             for data in test_dataset:        
                 data_clone = data.clone()
                 data_clone = data_clone.to(self.device)
-                out = self.model(data_clone)
+                out = self.model(data_clone.x, data_clone.pos)
 
                 targets = data_clone.y
                 loss_criterion = nn.MSELoss(reduction = 'none')
@@ -232,12 +182,7 @@ class BasicSimulator(nn.Module):
         return processed
 
 
-
-def custom_collate_fn(batch):
-    print("in Batch: ", batch)
-    return Data.from_data_list(batch)
-
-def global_train(device, train_dataset: DataLoader, network: GraphSAGE, hparams: dict, criterion='MSE', reg=1, local=False):
+def global_train(device, train_dataset: DataLoader, network, hparams: dict, criterion='MSE', reg=1, local=False):
     model = network.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=hparams['lr'])
     lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(
@@ -246,12 +191,12 @@ def global_train(device, train_dataset: DataLoader, network: GraphSAGE, hparams:
         total_steps=(len(train_dataset) // hparams['batch_size'] + 1) * hparams['nb_epochs'],
     )
 
-    train_loss_surf_list = []
-    train_loss_vol_list = []
-    loss_surf_var_list = []
-    loss_vol_var_list = []
+    best_loss = float('inf')
+    patience = hparams.get('patience', 5)  # Set default patience if not provided
+    patience_counter = 0
+
     train_loss_list = []
-    
+
     pbar_train = tqdm(range(hparams['nb_epochs']), position=0)
     epoch_nb = 0
 
@@ -259,25 +204,25 @@ def global_train(device, train_dataset: DataLoader, network: GraphSAGE, hparams:
         epoch_nb += 1
         print('Epoch: ', epoch_nb)
 
-        train_loss, _, loss_surf_var, loss_vol_var, loss_surf, loss_vol = train_model(device, model, train_dataset, optimizer, lr_scheduler, criterion, reg=reg)
-        if criterion == 'MSE_weighted':
-            train_loss = reg * loss_surf + loss_vol
+        train_loss, _, _, _, _, _ = train_model(device, model, train_dataset, optimizer, lr_scheduler, criterion, reg=reg)
+
         train_loss_list.append(train_loss)
-        train_loss_surf_list.append(loss_surf)
-        train_loss_vol_list.append(loss_vol)
-        loss_surf_var_list.append(loss_surf_var)
-        loss_vol_var_list.append(loss_vol_var)
 
-        f = open("losses.txt", "a")
-        f.write("Epoch: " + str(epoch) + " loss: " + str(train_loss) + " loss_surf: " + str(loss_surf) + " loss_vol: " + str(loss_vol) + "\n")
-        f.close()
-        
-    loss_surf_var_list = np.array(loss_surf_var_list)
-    loss_vol_var_list = np.array(loss_vol_var_list)
+        # Check for early stopping
+        if train_loss < best_loss:
+            best_loss = train_loss
+            patience_counter = 0  # Reset counter if there's improvement
+            # Save the model or any other actions
+            torch.save(model.state_dict(), "best_model.pth")
+        else:
+            patience_counter += 1  # Increment counter if no improvement
 
-    # Plotting the loss
-    plt.plot(train_loss_list, label='Total Loss')
-    plt.savefig('total_loss.png')
+        if patience_counter >= patience:
+            print(f"Early stopping activated. No improvement for {patience} epochs.")
+            break
+
+    # Load the best model
+    model.load_state_dict(torch.load("best_model.pth"))
     
     return model
     
@@ -297,7 +242,7 @@ def train_model(device, model, train_loader, optimizer, scheduler, criterion = '
         data_clone = data.clone()
         data_clone = data_clone.to(device)   
         optimizer.zero_grad()  
-        out = model(data_clone)
+        out = model(data_clone.x, data_clone.pos)
         targets = data_clone.y.to(device)
 
         if criterion == 'MSE' or criterion == 'MSE_weighted':
@@ -310,7 +255,6 @@ def train_model(device, model, train_loader, optimizer, scheduler, criterion = '
         loss_vol_var = loss_criterion(out[~data_clone.surf, :], targets[~data_clone.surf, :]).mean(dim = 0)
         loss_surf = loss_surf_var.mean()
         loss_vol = loss_vol_var.mean()
-
         if criterion == 'MSE_weighted':            
             (loss_vol + reg*loss_surf).backward()      
         else:
@@ -325,8 +269,10 @@ def train_model(device, model, train_loader, optimizer, scheduler, criterion = '
         avg_loss_surf += loss_surf
         avg_loss_vol += loss_vol 
         iterNum += 1
+    print("GPU memory allocated: ", torch.cuda.memory_allocated(device)/1e9, "GB, Max memory allocated: ", torch.cuda.max_memory_allocated(device)/1e9, "GB")
     print("Time for epoch: ", time.time()-start)
     print("Loss: ", avg_loss.cpu().data.numpy()/iterNum)
 
     return avg_loss.cpu().data.numpy()/iterNum, avg_loss_per_var.cpu().data.numpy()/iterNum, avg_loss_surf_var.cpu().data.numpy()/iterNum, avg_loss_vol_var.cpu().data.numpy()/iterNum, \
             avg_loss_surf.cpu().data.numpy()/iterNum, avg_loss_vol.cpu().data.numpy()/iterNum
+
