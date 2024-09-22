844a5ad8b1e2973bbb14cb61fe0e6a4820b18cae
DimitriTimoz <dimitri.timoz@protonmail.com>
2024-09-22 22:21:22 +0200

    Set to device

--- models/graphd/basic.py
+++ models/graphd/basic.py
@@ -102,7 +102,7 @@ class BasicSimulator(nn.Module):
             sampleData = Data(pos=simulation_positions,
                             x=simulation_features, 
                             y=simulation_labels,
-                            edge_index=edges[name[0]],
+                            edge_index=Tensor(edges[name[0]]).to(self.device),
                             surf = simulation_surface.bool()) 
             torchDataset.append(sampleData)
             start_index += nb_nodes_in_simulation
