164560096c39d49e1dff98d1ad473e254669034d
DimitriTimoz <dimitri.timoz@protonmail.com>
2024-10-03 22:54:00 +0200

    Le bug de la mort

--- models/meshup/test.py
+++ models/meshup/test.py
@@ -9,21 +9,20 @@ import torch
 import matplotlib.pyplot as plt
 from utils import DelaunayTransform
 from torch_geometric.data import Data
-import networkx as nx
 import pyvista as pv
 import numpy as np
-torch.__version__
+torch.__version__, pv.__version__
 
 # %%
 import random
-N = 50_000
+N = 150_000
 pos = torch.rand((N, 2))
 data = Data(pos=pos, surf=torch.full((N, 1), False))
 transform = DelaunayTransform()
 data = transform(data)
 data.pos = pos
 
-#data = torch.load('./sampleData.pth')
+data = torch.load('./sampleData.pth')
 
 def plot_graph(data, l=1, plotter=None):
     c = ['r', 'g', 'b', 'm']
@@ -38,18 +37,28 @@ def plot_graph(data, l=1, plotter=None):
     edges = data.edge_index.t().numpy()
     lines = np.hstack([np.full((edges.shape[0], 1), 2), edges]).ravel()
     mesh.lines = lines
-    p.add_mesh(mesh, line_width=1, color=random.choice(c))
+    p.add_mesh(mesh, color=random.choice(c))
     
     if plotter is None:
         p.show()
 
 # %%
-#plot_graph(data)
+plot_graph(data)
 
 # %%
 import time
+# TODO/ vérifier les direction des edges dans le papier
+def divide_mesh(v: torch.Tensor, e: torch.Tensor, k: int):
+    """Divide a mesh into k clusters of edges according to their direction.
 
-def divide_mesh(v, e, k):
+    Args:
+        v (Tensor(N, 2)): Positions of the vertices in the mesh.
+        e (Tensor(2, N)): Edge indices of the mesh.
+        k (int): Number of clusters to divide the mesh into.
+
+    Returns:
+        
+    """
     clusters = [Data(edge_ids=set()) for _ in range(k)]
     
     # Randomly initialize centroids (2D points)
@@ -82,8 +91,6 @@ def divide_mesh(v, e, k):
                 n_m = max(torch.norm(centroids[i] - last_centroid), n_m)
         norm_changes = n_m
 
-    print("All:", time.time() - start_all)
-
     # Post-process clusters to finalize edge indices
     for cluster in clusters:
         cluster.edge_index = e[list(cluster.edge_ids)]
@@ -98,135 +105,64 @@ clusters = divide_mesh(data.pos, data.edge_index.T, 8)
 clusters
 
 # %%
-import numpy as np
+%%time
+
 import torch
-from torch_geometric.data import Data
-from sklearn.cluster import MiniBatchKMeans
+from torch_cluster import grid_cluster
+from torch_scatter import scatter
 
-def generate_coarse_graph_with_clustering(g, num_clusters):
+def generate_coarse_graph(data, r):
+    """Generate a coarse graph from a fine graph.
+
+    Args:
+        data (Data): The fine graph to coarsen.
+        r (_type_): _description_
+
+    Returns:
+        _type_: _description_
     """
-    Generates a coarser graph from a given graph using MiniBatchKMeans clustering
-    to reduce the number of nodes. Returns the coarse graph and the mapping from
-    original nodes to clusters.
+    #FIXME: use square grid 
+    size = torch.max(data.pos, dim=0)[0] - torch.min(data.pos, dim=0)[0]
 
-    :param g: Original graph (of type torch_geometric Data).
-    :param num_clusters: The number of clusters or "super-nodes" to create.
-    :return: A tuple (coarse_graph, labels) where labels map original nodes to clusters.
-    """
-    pos = g.pos.cpu().numpy()  
-    edges = g.edge_index.cpu().numpy() 
-
-    # Use MiniBatchKMeans
-    kmeans = MiniBatchKMeans(n_clusters=num_clusters, batch_size=10000, random_state=0).fit(pos)
-    cluster_centers = kmeans.cluster_centers_
-    labels = kmeans.labels_
-
-    # Create new edges by connecting clusters if a link exists between two nodes in the original graph
-    new_edges = set()
-    for edge in edges.T:
-        cluster_u = labels[edge[0]]
-        cluster_v = labels[edge[1]]
-        if cluster_u != cluster_v:
-            new_edges.add((min(cluster_u, cluster_v), max(cluster_u, cluster_v)))
-
-    # Convert new edges to torch tensor
-    if new_edges:
-        new_edges = np.array(list(new_edges)).T
-        new_edges = torch.tensor(new_edges, dtype=torch.long)
-    else:
-        new_edges = torch.empty((2, 0), dtype=torch.long)
+    size /= r
     
-    # Convert cluster centers to tensor
-    new_pos = torch.tensor(cluster_centers, dtype=torch.float)
-
-    # Create new coarse graph
-    coarse_graph = Data(pos=new_pos, edge_index=new_edges)
+    # Assign each node to a grid cell id
+    cluster = grid_cluster(data.pos, size) 
     
-    return coarse_graph, labels  # Return the mapping
+    # Get the indices of the unique clusters
+    unique_indices, new_index = torch.unique(cluster, return_inverse=True)
+    # Gather each node to its cluster and compute the mean for position features
+    out_positions = scatter(data.pos.t(), new_index, reduce='mean')
+    
+    out_x = scatter(data.x.t(), new_index, reduce='mean')
+    
 
-# Now, generate the hierarchical mesh
-graphs = [data]  # List of graphs, starting with the original graph
-mappings = []    # List of mappings from nodes to clusters at each level
+    # Interpolate the other features accordingly to the position    
+    surf = scatter(data.surf.to(torch.int), new_index, reduce='max')
+    edge_index = torch.stack([new_index+data.num_nodes, torch.arange(0, new_index.shape[0])], dim=0)
+    
+    #edge_clusters = divide_mesh(out_positions.t(), edge_index, 6)
+    return transform(Data(pos=out_positions.t()[:, :2], x=out_x.t(), surf=surf)), edge_index
+    
+def generate_coarse_graphs(data, R: int):
+    range_ = 5000
+    base = data.clone()
+    base.pos = torch.concatenate([base.pos, torch.full((base.pos.shape[0], 1), 1)], axis=1)
+    for i in range(2, R+1):
+        subgraph, connection_index = generate_coarse_graph(data, range_//(5**i)) # TODO: choose the right scale factor
+        subgraph.pos = torch.concatenate([subgraph.pos, torch.full((subgraph.pos.shape[0], 1), i)], axis=1)
+        subgraph.edge_index = torch.add(subgraph.edge_index, base.pos.shape[0])
 
-current_graph = data
-num_levels = 3  # Define the number of levels you want
-for level in range(num_levels):
-    num_clusters = int(len(current_graph.pos) // 10)
-    print(f"Generating coarse graph at level {level+1} with {num_clusters} clusters...")
-    coarse_graph, mapping = generate_coarse_graph_with_clustering(current_graph, num_clusters)
-    graphs.append(coarse_graph)
-    mappings.append(mapping)
-    current_graph = coarse_graph
+        connection_index = torch.add(connection_index, base.pos.shape[0])
+        data = subgraph.clone()
+        base.pos = torch.cat([base.pos, subgraph.pos], dim=0)
 
-# Now, construct the final multi-layer graph
-all_positions = []
-all_edges = []
-node_offsets = []  # To keep track of node index offsets for each layer
+        base.edge_index = torch.cat([base.edge_index, subgraph.edge_index], dim=1)
+        base.edge_index = torch.cat([base.edge_index, connection_index ], dim=1)
 
-offset = 0
-z_offset = 1.0  # Vertical offset between layers
-for layer_index, graph in enumerate(graphs):
-    num_nodes = graph.pos.shape[0]
-    pos = graph.pos
-
-    # Adjust positions to add vertical dimension
-    if pos.shape[1] == 2:
-        # If positions are 2D, add a z-coordinate
-        z = torch.full((num_nodes, 1), layer_index * z_offset)
-        pos = torch.cat((pos, z), dim=1)
-    elif pos.shape[1] == 3:
-        # If positions are 3D, adjust the z-coordinate
-        pos[:, 2] += layer_index * z_offset
-    else:
-        raise ValueError("Position tensor has unsupported number of dimensions.")
-
-    all_positions.append(pos)
-    node_offsets.append(offset)
-    # Adjust edges
-    if graph.edge_index.numel() > 0:
-        adjusted_edges = graph.edge_index + offset
-        all_edges.append(adjusted_edges)
-    offset += num_nodes
-
-# Create inter-layer edges based on the mappings and deleted edges
-for level, mapping in enumerate(mappings):
-    # Nodes in layer n-1
-    start_idx = node_offsets[level]
-    end_idx = node_offsets[level] + len(mapping)
-    # Nodes in layer n
-    next_layer_start_idx = node_offsets[level+1]
-    # Map nodes from layer n-1 to layer n
-    node_indices = np.arange(start_idx, end_idx)
-    cluster_indices = mapping + next_layer_start_idx
-    inter_layer_edges = np.vstack((node_indices, cluster_indices))
-
-    # Add inter-layer edges to all_edges
-    all_edges.append(torch.tensor(inter_layer_edges, dtype=torch.long))
-
-    # Include deleted edges (edges between nodes merged into the same cluster)
-    # Identify edges in layer n-1 that were merged in layer n
-    edge_indices = graphs[level].edge_index.numpy()
-    for edge in edge_indices.T:
-        # edge[0] and edge[1] are local node indices (starting from 0)
-        if mapping[edge[0]] == mapping[edge[1]]:
-            # These nodes were merged; create an edge to the cluster center in layer n
-            cluster_idx = mapping[edge[0]] + next_layer_start_idx
-            # Adjust node indices by adding start_idx to align with global indices
-            all_edges.append(torch.tensor([[edge[0] + start_idx, cluster_idx],
-                                           [edge[1] + start_idx, cluster_idx]], dtype=torch.long))
-
-# Combine all positions and edges
-all_positions = torch.cat(all_positions, dim=0)
-all_edges = torch.cat(all_edges, dim=1)
-
-# Create final graph
-final_graph = Data(pos=all_positions, edge_index=all_edges)
-
-# Now you can plot the final graph
-p = pv.Plotter()
-print(final_graph)
-plot_graph(final_graph, 1, p)
-p.show()
-
+    return base
+b = generate_coarse_graphs(data, 3)
+print(b, flush=True)
+plot_graph(b)
 
 
