Fix min & max computation

models/meshup/my_model.py
@@ -268,7 +268,7 @@ class UaMgnn(nn.Module):
                 print("Nombre noeuds dans plus fin", pf_node_indices_range[1]-pf_node_indices_range[0], "range", pf_node_indices_range)
                 print("Nombre noeuds dans moins fin", mf_node_indices_range[1]-mf_node_indices_range[0], "range", mf_node_indices_range)
                 up_scale_edge_index = data.edge_index[:, up_scale_edge_range[0]:up_scale_edge_range[1]] #- pf_node_indices_range[0]
-                print("up_scale_edge_index", up_scale_edge_index.shape, up_scale_edge_index.min(), up_scale_edge_index.max()) 
+                print("up_scale_edge_index", up_scale_edge_index.shape, torch.min(up_scale_edge_index), torch.max(up_scale_edge_index)) 
                 print("nodes_embedding_up_sampling", nodes_embedding_up_sampling.shape)
                 node_embedding[pf_node_indices_range[0]:pf_node_indices_range[1]] = self.up_sampling_processors[ir](nodes_embedding_up_sampling, up_scale_edge_index , up_scale_edge_embeddings)
         

models/meshup/test.py
@@ -164,7 +164,7 @@ def generate_coarse_graphs(data, R: int, K: int, visualize=False):
         base.x = torch.cat([base.x, subgraph.x], dim=0)
         base.edge_index = torch.cat([base.edge_index, subgraph.edge_index, connection_index], dim=1)
         
-        base.up_scale_edge_ranges[i-2] = torch.tensor([base.edge_index.shape[0]-connection_index.shape[1], base.edge_index.shape[0]], device=base.pos.device) # TODO: check it
+        base.up_scale_edge_ranges[i-2] = torch.tensor([base.edge_index.shape[0]-connection_index.shape[1], base.edge_index.shape[0]], device=base.pos.device)
         base.layer_ranges[i-1] = torch.tensor([base.pos.shape[0]-subgraph.pos.shape[0]-1, base.pos.shape[0]], device=base.pos.device)
         
         if i >= R: